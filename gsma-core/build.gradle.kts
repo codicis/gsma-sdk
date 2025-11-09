4fa8630e9c83a276fbb2b897f60c996fbacfe530
codicis <codecis@icloud.com>
2025-11-09 11:15:18 +0100

    Clean up commented ASN.1 file list in `build.gradle.kts` and update `GPRSChargingDataTypes.asn` with proper imports.

--- gsma-core/build.gradle.kts
+++ gsma-core/build.gradle.kts
@@ -14,31 +14,6 @@ asn1 {
         register("compileAsn") {
             packageName.set("io.github.codicis.gsma")
             sourceFiles.from(fileTree("src/main/asn1"))
-                /*listOf(
-                    file("src/main/asn1/MAP-BS-Code.asn"),
-                    file("src/main/asn1/MAP-TS-Code.asn"),
-                    file("src/main/asn1/MAP-SS-Code.asn"),
-                    file("src/main/asn1/MAP-ExtensionDataTypes.asn"),
-                    file("src/main/asn1/MAP-CommonDataTypes.asn"),
-                    file("src/main/asn1/MAP-SS-DataTypes.asn"),
-                    file("src/main/asn1/MAP-OM-DataTypes.asn"),
-                    file("src/main/asn1/MAP-ER-DataTypes.asn"),
-                    file("src/main/asn1/MAP-SM-DataTypes.asn"),
-                    file("src/main/asn1/MAP-MS-DataTypes.asn"),
-                    file("src/main/asn1/MAP-LCS-DataTypes.asn"),
-                    file("src/main/asn1/MAP-GR-DataTypes.asn"),
-                    file("src/main/asn1/MAP-CH-DataTypes.asn"),
-                    file("src/main/asn1/SS-DataTypes.asn"),
-                    file("src/main/asn1/CS1-DataTypes.asn"),
-                    file("src/main/asn1/CS2-DataTypes.asn"),
-                    //file("src/main/asn1/Remote-Operations-Useful-Definitions.asn"),
-                    //file("src/main/asn1/Remote-Operations-Information-Objects.asn"),
-                    //file("src/main/asn1/Remote-Operations-Generic-ROS-PDUs.asn"),
-                    //file("src/main/asn1/MAP-Errors.asn"),
-                    //file("src/main/asn1/MAP-LocationServiceOperations.asn").
-                    //file("src/main/asn1/MAP-Group-Call-Operations.asn")
-                )
-            )*/
             outputDir.set(layout.buildDirectory.dir("generated/sources/asn1/$name"))
         }
     }
